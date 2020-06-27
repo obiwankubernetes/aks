de4ea49427657cba2f6f8f8f77d64e1583ba5ca0
Obi-Wan Kubernetes <64297764+obiwankubernetes@users.noreply.github.com>
2020-06-26 23:42:00 -0700

    update akscript

--- aksscript.py
+++ aksscript.py
@@ -12,14 +12,25 @@
         # docker login -u "username" -p "password" docker.io
     # push docker image to docker hub
         # docker-compose push python
-# create rg-> az group create --name myAKSResourceGroup --location eastus
-# create acr -> az acr create --resource-group myAKSResourceGroup --name pykub --sku Basic
-# login to acr -> az acr login --name pykub
-# retag local image for azure -> docker tag <localcontainer:image> <containername.azurecr.io>/obiwankubernetes/pyazure
-# create aks cluster -> az aks create --resource-group myAKSResourceGroup --name myAKSCluster --node-count 1 --generate-ssh-keys
-# get credentials of az aks get-credentials --resource-group myAKSResourceGroup --name myAKSCluster
-# check if it is running -> kubectl get nodes
-# prepare yaml manifest to deploy to aks
-# deploy yaml manifest -> kubectl apply -f <yaml-file-name>
-# get public facing url to test deployment and see app -> kubectl get service <appname> --watch
-# go to url
+
+# create kubernetes service/cluster in azure        
+    # create rg-> 
+        # az group create --name myAKSResourceGroup --location eastus
+    # create acr -> 
+        # az acr create --resource-group myAKSResourceGroup --name pykub --sku Basic
+    # login to acr -> 
+        # az acr login --name pykub
+    # retag local image for azure -> 
+        # docker tag <localcontainer:image> <containername.azurecr.io>/obiwankubernetes/pyazure
+    # create aks cluster -> 
+        # az aks create --resource-group myAKSResourceGroup --name myAKSCluster --node-count 1 --generate-ssh-keys
+    # get credentials ->
+        #az aks get-credentials --resource-group myAKSResourceGroup --name myAKSCluster
+    # check if it is running -> 
+        # kubectl get nodes
+    # prepare yaml manifest to deploy to aks
+    # deploy yaml manifest -> 
+        # kubectl apply -f <yaml-file-name>
+    # get public facing url to test deployment and see app -> 
+        #kubectl get service <appname> --watch
+    # go to url
